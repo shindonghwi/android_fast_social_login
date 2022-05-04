f0a3751ba0e9f03848a5a1acd62581abd9a3b14b
shindonghwi <ehdgnl8940@naver.com>
2022-05-04 17:48:36 +0900

    feat: kakao login, logout method 틀 제작

---
+++ simple_social_login/src/main/java/wolf/shin/simple_social_login/kakao/KakaoLoginHelper.kt
@@ -0,0 +1,24 @@
+package wolf.shin.simple_social_login.kakao
+
+import com.kakao.sdk.auth.model.OAuthToken
+
+class KakaoLoginHelper {
+
+    val kakaoCallback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+        if (error != null) {
+            // fail
+        } else if (token != null) {
+            // success
+        }
+    }
+
+    // kakao Login
+    fun kakaoLogin() {
+    }
+
+    // kakao Logout
+    fun kakaoLogout() {
+
+    }
+
+}
