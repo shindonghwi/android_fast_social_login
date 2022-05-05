2fdf5026898a5c01b51eb70d92386e6c1848f986
shindonghwi <ehdgnl8940@naver.com>
2022-05-06 02:19:09 +0900

    feat: 카카오 로그인 ui 제작

--- app/src/main/java/wolf/shin/simplesociallogin/MainActivity.kt
+++ app/src/main/java/wolf/shin/simplesociallogin/MainActivity.kt
@@ -3,21 +3,49 @@ package wolf.shin.simplesociallogin
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import wolf.shin.simple_social_login.SimpleSocialLoginSDK
 import wolf.shin.simplesociallogin.ui.theme.SimpleSocialLoginTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val simpleSocialLoginSDK by lazy { SimpleSocialLoginSDK.Builder(this) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SimpleSocialLoginTheme {
 
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(200.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        modifier = Modifier
+                            .padding(all = 24.dp)
+                            .background(Color.Yellow)
+                            .padding(all = 12.dp)
+                            .clickable {
+                                simpleSocialLoginSDK.doKakaoLogin()
+                            },
+                        text = "카카오로그인",
+                        style = MaterialTheme.typography.h4,
+                        color = Color.Black
+                    )
+
+                }
             }
         }
     }
