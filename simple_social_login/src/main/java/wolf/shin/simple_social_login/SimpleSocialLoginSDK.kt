59bc9e5be14cfd5a8fceceaba23e6531d5e2608c
shindonghwi <ehdgnl8940@naver.com>
2022-05-06 02:48:50 +0900

    feat: 카카오 로그인, 로그아웃, 연결해제 기능 추가

--- simple_social_login/src/main/java/wolf/shin/simple_social_login/SimpleSocialLoginSDK.kt
+++ simple_social_login/src/main/java/wolf/shin/simple_social_login/SimpleSocialLoginSDK.kt
@@ -1,21 +1,53 @@
 package wolf.shin.simple_social_login
 
-import android.content.Context
-import android.os.StatFs
+import android.app.Activity
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import wolf.shin.simple_social_login.kakao.KakaoLoginHelper
 import wolf.shin.simple_social_login.model.LoginState
+import wolf.shin.simple_social_login.model.LogoutState
+import wolf.shin.simple_social_login.model.UnlinkState
 
-class SimpleSocialLoginSDK{
 
-    class Builder(private val context: Context) {
+class SimpleSocialLoginSDK {
+
+    class Builder(
+        private val activity: Activity
+    ) {
+        private val kakaoLoginHelper: KakaoLoginHelper = KakaoLoginHelper(activity)
 
         // 로그인 상태를 관찰 할 수 있는 Flow 생성
         private val _loginStateFlow = MutableStateFlow<LoginState>(LoginState.Init)
         val loginStateFlow: StateFlow<LoginState> get() = _loginStateFlow
 
-        fun doKakaoLogin(){
+        // 로그아웃 상태를 관찰 할 수 있는 Flow 생성
+        private val _logoutStateFlow = MutableStateFlow<LogoutState>(LogoutState.Init)
+        val logoutStateFlow: StateFlow<LogoutState> get() = _logoutStateFlow
 
+        // 연결해제 상태를 관찰 할 수 있는 Flow 생성
+        private val _unlinkStateFlow = MutableStateFlow<UnlinkState>(UnlinkState.Init)
+        val unlinkStateFlow: StateFlow<UnlinkState> get() = _unlinkStateFlow
+
+
+        /**
+         * #########################
+         * #### K A K A O
+         * #########################
+         * */
+
+        suspend fun doKakaoLogin(): StateFlow<LoginState> {
+            kakaoLoginHelper.doKakaoLogin(_loginStateFlow)
+            return loginStateFlow
+        }
+
+        suspend fun doKakaoLogout(): StateFlow<LogoutState>{
+            kakaoLoginHelper.doKakaoLogout(_logoutStateFlow)
+            return logoutStateFlow
+        }
+
+        suspend fun doKakaoUnlink(): StateFlow<UnlinkState> {
+            kakaoLoginHelper.doUnlink(_unlinkStateFlow)
+            return unlinkStateFlow
         }
 
     }
