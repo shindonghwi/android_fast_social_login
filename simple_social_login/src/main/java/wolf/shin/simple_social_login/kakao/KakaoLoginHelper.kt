ba73f69603489a749e3d43c2063afb3dcac2496a
shindonghwi <ehdgnl8940@naver.com>
2022-05-06 02:47:21 +0900

    feat: kakao 로그인 헬퍼 클래스 생성

--- simple_social_login/src/main/java/wolf/shin/simple_social_login/kakao/KakaoLoginHelper.kt
+++ simple_social_login/src/main/java/wolf/shin/simple_social_login/kakao/KakaoLoginHelper.kt
@@ -1,25 +1,79 @@
 package wolf.shin.simple_social_login.kakao
 
-import com.kakao.sdk.auth.model.OAuthToken
+import android.app.Activity
+import com.kakao.sdk.common.KakaoSdk
+import com.kakao.sdk.common.model.AuthError
+import com.kakao.sdk.user.UserApiClient
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.runBlocking
+import wolf.shin.simple_social_login.BuildConfig
+import wolf.shin.simple_social_login.model.LoginState
+import wolf.shin.simple_social_login.model.LogoutState
+import wolf.shin.simple_social_login.model.UnlinkState
 
-class KakaoLoginHelper {
 
-    val kakaoCallback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
-        if (error != null) {
-            com.kakao.sdk.auth.AuthCodeHandlerActivity().intent
-        // fail
-        } else if (token != null) {
-            // success
+class KakaoLoginHelper(
+    private val activity: Activity
+) : iKakaoLoginApi {
+
+    init {
+        KakaoSdk.init(activity, BuildConfig.KAKAO_APP_KEY)
+    }
+
+    /** 카카오 로그인 */
+    override suspend fun doKakaoLogin(loginFlow: MutableStateFlow<LoginState>) {
+        loginFlow.emit(LoginState.Loading)
+        with(activity) {
+            if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
+                UserApiClient.instance.loginWithKakaoTalk(this) { token, error: Throwable? ->
+                    if (error != null) {
+                        if (error is AuthError && error.statusCode == 302) {
+                            runBlocking { loginFlow.emit(LoginState.Error(message = "$error")) }
+                            return@loginWithKakaoTalk
+                        }
+                        return@loginWithKakaoTalk
+                    }
+
+                    if (token != null) {
+                        runBlocking {  LoginState.Success(token = token.accessToken) }
+                    }
+                }
+            } else {
+                UserApiClient.instance.loginWithKakaoAccount(this) { token, error: Throwable? ->
+                    if (error != null) {
+                        runBlocking { loginFlow.emit(LoginState.Error(message = "$error")) }
+                        return@loginWithKakaoAccount
+                    }
+                    if (token != null) {
+                        runBlocking {  LoginState.Success(token = token.accessToken) }
+                    }
+                }
+            }
         }
     }
 
-    // kakao Login
-    fun kakaoLogin() {
+    /** 카카오 로그아웃 */
+    override suspend fun doKakaoLogout(logoutFlow: MutableStateFlow<LogoutState>) {
+        logoutFlow.value = LogoutState.Loading
+        UserApiClient.instance.logout { error: Throwable? ->
+            if (error != null) {
+                logoutFlow.value = LogoutState.Error(message = error.message)
+                return@logout
+            }
+            logoutFlow.value = LogoutState.Success
+        }
     }
 
-    // kakao Logout
-    fun kakaoLogout() {
-
+    /** 카카오 연결해제 */
+    override suspend fun doUnlink(unlinkFlow: MutableStateFlow<UnlinkState>) {
+        unlinkFlow.value = UnlinkState.Loading
+        UserApiClient.instance.unlink { error: Throwable? ->
+            if (error != null) {
+                unlinkFlow.value = UnlinkState.Error(message = error.message)
+                return@unlink
+            }
+            unlinkFlow.value = UnlinkState.Success
+        }
     }
 
 }
