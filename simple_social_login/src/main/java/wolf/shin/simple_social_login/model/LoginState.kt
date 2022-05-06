905136cc65e60138a68baeef94a84fdb6e1d1891
shindonghwi <ehdgnl8940@naver.com>
2022-05-06 14:53:22 +0900

    fix: LoginState 자료형 변경

--- simple_social_login/src/main/java/wolf/shin/simple_social_login/model/LoginState.kt
+++ simple_social_login/src/main/java/wolf/shin/simple_social_login/model/LoginState.kt
@@ -1,8 +1,8 @@
 package wolf.shin.simple_social_login.model
 
-sealed class LoginState{
-    object Init: LoginState()
-    object Loading: LoginState()
-    data class Success(val token: String): LoginState()
-    data class Error(val message: String?): LoginState()
+sealed class LoginState<out T> {
+    object Init : LoginState<Nothing>()
+    object Loading : LoginState<Nothing>()
+    data class Success(val token: String) : LoginState<String>()
+    data class Error(val message: String?) : LoginState<Nothing>()
 }
