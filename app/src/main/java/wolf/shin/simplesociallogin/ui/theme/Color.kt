ce0e191e9877317c0b69d43888b0d4b6abeb659e
shindonghwi <ehdgnl8940@naver.com>
2022-05-07 01:48:03 +0900

    feat: Gray300 add

--- app/src/main/java/wolf/shin/simplesociallogin/ui/theme/Color.kt
+++ app/src/main/java/wolf/shin/simplesociallogin/ui/theme/Color.kt
@@ -7,6 +7,7 @@ val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
 
+val Gray300 = Color(0xFFD1D5DB)
 val Gray800 = Color(0xFF212936)
 val OutLine60 = Color(0x14171A22)
 val KakaoColor = Color(0xFFFEE500)
