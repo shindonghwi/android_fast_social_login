7d912bf1040956f49c103697a0b59a02369ebd24
shindonghwi <ehdgnl8940@naver.com>
2022-05-11 12:25:23 +0900

    refactor: 사용하지 않는 파일 제거

--- simple_social_login/src/main/java/wolf/shin/simple_social_login/model/ActivityRequest.kt
+++
@@ -1,8 +0,0 @@
-package wolf.shin.simple_social_login.model
-
-import android.content.Intent
-
-data class ActivityRequest(
-    val intent: Intent,
-    val listener: OnActivityResultListener
-)
--- simple_social_login/src/main/java/wolf/shin/simple_social_login/model/OnActivityResultListener.kt
+++
@@ -1,7 +0,0 @@
-package wolf.shin.simple_social_login.model
-
-import android.content.Intent
-
-interface OnActivityResultListener {
-    fun onActivityResult(resultCode: Int, intent: Intent?)
-}
